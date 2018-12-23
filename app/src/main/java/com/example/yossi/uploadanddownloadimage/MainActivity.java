f5b1018cab89efe62a78fef18662b1477dc16cad
yossi <yossi445@gmail.com>
2018-12-23 14:50:55 +0200

    uploadfile() and saving user

--- app/src/main/java/com/example/yossi/uploadanddownloadimage/MainActivity.java
+++ app/src/main/java/com/example/yossi/uploadanddownloadimage/MainActivity.java
@@ -1,21 +1,31 @@
 package com.example.yossi.uploadanddownloadimage;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btn;
-    private StorageReference mStorageRef;
+     Button btn;
+     StorageReference mStorageRef;
+     DatabaseReference databaseReference;
+
+     Uri imageUri;
 
 
-    static final int PICK_IMAGE_REQUEST = 1;
+     static final int PICK_IMAGE_REQUEST = 1;
 
 
     @Override
@@ -26,7 +36,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btn = findViewById(R.id.btn);
         btn.setOnClickListener(this);
-        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        mStorageRef = FirebaseStorage.getInstance().getReference("uploads");
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
     }
 
     @Override
@@ -41,5 +53,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK)
+        {
+            imageUri = data.getData();
+
+            uploadFile();
+        }
+    }
+
+    private void uploadFile() {
+
+        StorageReference fileRef = mStorageRef.child(String.valueOf(System.currentTimeMillis()));
+
+        fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                Toast.makeText(MainActivity.this, "upload complete", Toast.LENGTH_SHORT).show();
+                User user = new User("popo",taskSnapshot.getMetadata().getReference().getDownloadUrl().toString());
+
+                String userId = databaseReference.push().getKey();
+                databaseReference.child(userId).setValue(user);
+
+            }
+        });
     }
 }
