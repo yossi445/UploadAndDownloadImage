297681b9a50b1652873727bf342a3d0521ec0294
yossi <yossi445@gmail.com>
2018-12-23 11:56:37 +0200

    layout anf opening filechooser

---
+++ app/src/main/java/com/example/yossi/uploadanddownloadimage/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.yossi.uploadanddownloadimage;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn;
+    private StorageReference mStorageRef;
+
+
+    static final int PICK_IMAGE_REQUEST = 1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(this);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        openFileChooser();
+    }
+
+    private void openFileChooser() {
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+}
---
+++ app/src/main/java/com/example/yossi/uploadanddownloadimage/User.java
@@ -0,0 +1,18 @@
+package com.example.yossi.uploadanddownloadimage;
+
+public class User {
+
+    String name;
+    String profileImageUrl;
+
+    public User(){
+
+    }
+
+    public User(String name, String profileImageUrl) {
+        this.name = name;
+        this.profileImageUrl = profileImageUrl;
+    }
+
+
+}
